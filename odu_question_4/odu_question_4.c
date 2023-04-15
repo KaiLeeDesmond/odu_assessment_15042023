9a21126b0012e4184b1d44895d40fc3dad066d5c
Amogh Krishna <72075341+KaiLeeDesmond@users.noreply.github.com>
2023-04-16 05:15:40 +0530

    Add files via upload

---
+++ odu_question_4/odu_question_4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int c=0,c1=0,x=0;
+    //declaring variables and initializing to 0
+    char str[111];
+    printf("Enter a string please");
+    scanf("%s",&str);
+    //receiving input from user 
+    for(int i=0;i<str[i]!='\0';i++) //loop that iterates through the string
+    {
+        if(str[i]==str[i+1])//comparing the character at the ith position with the character at the (i+1)th position
+        {
+            c++;//incrementing the counter variable
+            x=i+1;//storing i+1 to compare with the next character
+        }
+
+        if(str[x]==str[x+1])//comparing the character at the xth position with the character at the (x+1)th position 
+            c1++;//incrementing the second counter variable
+
+    }
+    if(c>c1)
+        printf("%d",c);//displaying c if c is greater
+    else
+        printf("%d",c1);//displaying c1 if c1 is greater
+return 0;
+}
